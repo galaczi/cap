3aa82233208bd04fd5ea4a240aef263f1c7867d1
Jeno Galaczi <galaczi@gmail.com>
2022-11-28 17:20:59 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+// puppeteer-extra is a drop-in replacement for puppeteer,
+// it augments the installed puppeteer with plugin functionality
+const puppeteer = require('puppeteer-extra')
+const {executablePath} = require('puppeteer')
+
+// add recaptcha plugin and provide it your 2captcha token (= their apiKey)
+// 2captcha is the builtin solution provider but others would work as well.
+// Please note: You need to add funds to your 2captcha account for this to work
+const RecaptchaPlugin = require('puppeteer-extra-plugin-recaptcha')
+puppeteer.use(
+  RecaptchaPlugin({
+    provider: {
+      id: '2captcha',
+      token: 'CAI-C9F4D58788BD8CED08DEF149013516E1' // REPLACE THIS WITH YOUR OWN 2CAPTCHA API KEY ⚡
+    },
+    visualFeedback: true // colorize reCAPTCHAs (violet = detected, green = solved)
+  })
+)
+
+// puppeteer usage as normal
+puppeteer.launch({ 
+  headless: false,
+  executablePath: executablePath(),
+}).then(async browser => {
+  const page = await browser.newPage()
+  await page.goto('https://www.google.com/recaptcha/api2/demo')
+
+  // That's it, a single line of code to solve reCAPTCHAs 🎉
+  await page.solveRecaptchas()
+
+  await Promise.all([
+    page.waitForNavigation(),
+    page.click(`#recaptcha-demo-submit`)
+  ])
+  await page.screenshot({ path: 'response.png', fullPage: true })
+  await browser.close()
+})
